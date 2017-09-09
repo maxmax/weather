80e359efc344c0396d648b0883a6be3a9c759b9f
maxmax <maxmax@MacBook-Pro-maxmax.local>
2017-09-09 05:40:09 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class WeatherDisplay extends Component {
 
   componentDidMount() {
     const zip = this.props.zip;
-    const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
+    const URL = "https://api.openweathermap.org/data/2.5/weather?q=" +
       zip +
       "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial";
     fetch(URL).then(res => res.json()).then(json => {
@@ -33,9 +33,9 @@ class WeatherDisplay extends Component {
   render() {
 
     const weatherData = this.state.weatherData;
-    if (!weatherData) return <div>Loading</div>;
+    if (!weatherData) return <div className="loader"></div>;
     const weather = weatherData.weather[0];
-    const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+    const iconUrl = "https://openweathermap.org/img/w/" + weather.icon + ".png";
 
     return (
       <div>
